966a0bf55fd6f57bfbb354ffb8e4067a97b82ded
CosminBM <cosmincoco88@gmail.com>
2018-12-11 21:39:19 +0200

    Update-1

--- Challenge1SV/src/com/cosmin/Main.java
+++ Challenge1SV/src/com/cosmin/Main.java
@@ -1,38 +1,36 @@
 package com.cosmin;
 
 
-
 public class Main {
+
 
     public static void main(String[] args) {
 
-        int array[] = {5, 7, 2, 4, 9};
+        int array[] = {5, 7, 2, 4, 8, 9, 6};
+        int min = array[0];
+        int max = 0;
 
-        System.out.println("Method 1");
-        System.out.println(java.util.Arrays.toString(array));
-        System.out.println("The difference between the largest and smallest element is: " + (array[4] - array[2]));
-        System.out.println();
 
-        System.out.println("----------");
-
-        System.out.println();
-
-        System.out.println("Method 2");
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + "  ");
+            System.out.print(array[i] + " ");
+
+            if (min > array[i]) {
+
+                min = array[i];
+
+            } else if (max < array[i]) {
+
+                max = array[i];
+
+            }
 
         }
-        System.out.println();
-        System.out.println("The difference between the largest and smallest element is: " + (array[4] - array[2]));
-        System.out.println();
-
-        System.out.println("----------");
 
         System.out.println();
-
-        System.out.println("Method 3");
-        System.out.println(array[0] + " , " + array[1] + " , " + array[2] + " , " + array[3] + " , " + array[4]);
-        System.out.println("The difference between the largest and smallest element is: " + (array[4] - array[2]));
+        int diff = max - min;
+        System.out.println("The difference between max and min is: " + diff);
 
     }
+
 }
+
