ffc42887b8e2ff83d5fc0dcbdcee9767b9fc153f
CosminBM <cosmincoco88@gmail.com>
2018-12-19 16:46:16 +0200

    Update1

---
+++ Challenge2SV/src/com/cosmin/Main.java
@@ -0,0 +1,36 @@
+package com.cosmin;
+
+import java.util.*;
+
+
+public class Main {
+
+
+
+    public static void getInput() {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type some letters: ");
+        String inputText = reader.nextLine();
+        System.out.println("You typed: " + inputText);
+
+    }
+
+    public static void addInput() {
+        Dictionary chars = new Hashtable<Integer, String>();
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        getInput();
+
+
+
+
+    }
+}
