722c3ef4fcbd5373f258c9cfd4b849eea770d623
CosminBM <cosmincoco88@gmail.com>
2019-01-31 14:04:17 +0200

    FirstCommit

---
+++ Challenge4SV/src/com/cosmin/Alien.java
@@ -0,0 +1,4 @@
+package com.cosmin;
+
+public class Alien {
+}
---
+++ Challenge4SV/src/com/cosmin/TestGame.java
@@ -0,0 +1,8 @@
+package com.cosmin;
+
+public class TestGame {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
