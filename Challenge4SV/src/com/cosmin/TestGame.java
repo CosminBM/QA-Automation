20f5ca8dfc5a37063acd27495c0375b42b34b711
CosminBM <cosmincoco88@gmail.com>
2019-02-01 12:17:35 +0200

    GameOver!

--- Challenge4SV/src/com/cosmin/TestGame.java
+++ Challenge4SV/src/com/cosmin/TestGame.java
@@ -6,44 +6,59 @@ public class TestGame {
         Alien predator = new Alien(100, 200);
         Human man = new Human(200, 100);
 
-        while ((predator.getEnergy() > 0 && predator.getHealth() > 0) && (man.getAmmo() > 0 && man.getHealth() > 0)) {
-            if ((predator.getEnergy() >= 10 && predator.getHealth() > 0) && (man.getAmmo() >= 15 && man.getHealth() > 0)) {
-
+        while (predator.getHealth() != 0 && man.getHealth() != 0) {
+            if (predator.getEnergy() < 10) {
+                predator.setEnergy(predator.getEnergy());
+                System.out.println("Alien's energy:" + predator.getEnergy());
+                System.out.println("Energy is lower than 10, can't byte!");
+                break;
+            } else if (predator.getHealth() <= 0) {
+                predator.setDead(predator.isDead());
+                System.out.println("Alien's health: " + predator.getHealth());
+                System.out.println("Alien has died!");
+                break;
+            } else {
                 predator.biteHuman(man);
                 System.out.println("Alien bites man");
                 System.out.println("Alien's energy:" + predator.getEnergy());
                 System.out.println("Human's health: " + man.getHealth());
                 System.out.println("-----------------------");
-                if (predator.getEnergy() < 10) {
-                    predator.setEnergy(predator.getEnergy());
-                    System.out.println("Alien's energy:" + predator.getEnergy());
-                    System.out.println("Energy is lower than 15, can't byte!");
-                } else if (predator.getHealth() <= 0) {
-                    predator.setDead(predator.isDead());
-                    System.out.println("Alien's health: " + predator.getHealth());
-                    System.out.println("Alien has died!");
-                }
+            }
 
+            if (man.getAmmo() < 15) {
+                man.setAmmo(man.getAmmo());
+                System.out.println("Human's ammo:" + man.getAmmo());
+                System.out.println("Ammo is lower than 15, can't shoot!");
+                break;
+            } else if (man.getHealth() <= 0) {
+                man.setDead(man.isDead());
+                System.out.println("Human's health: " + man.getHealth());
+                System.out.println("Human has died!");
+                break;
+            } else {
                 man.shootAlien(predator);
-                System.out.println("Human shoots Alien one time");
+                System.out.println("Human shoots Alien first time");
                 System.out.println("Human's ammo:" + man.getAmmo());
                 System.out.println("Alien's health: " + predator.getHealth());
                 System.out.println("-----------------------");
-                if (man.getAmmo() < 15) {
-                    man.setAmmo(man.getAmmo());
-                    System.out.println("Human's ammo:" + man.getAmmo());
-                    System.out.println("Ammo is lower than 15, can't shoot!");
-                } else if (man.getHealth() <= 0) {
-                    man.setDead(man.isDead());
-                    System.out.println("Human's health: " + man.getHealth());
-                    System.out.println("Human has died!");
-                } else {
-                    man.shootAlien(predator);
-                    System.out.println("Human shoots Alien one time");
-                    System.out.println("Human's ammo:" + man.getAmmo());
-                    System.out.println("Alien's health: " + predator.getHealth());
-                    System.out.println("-----------------------");
-                }
+            }
+
+            if (man.getAmmo() < 15) {
+                man.setAmmo(man.getAmmo());
+                System.out.println("Human's ammo:" + man.getAmmo());
+                System.out.println("Ammo is lower than 15, can't shoot!");
+                break;
+            } else if (man.getHealth() <= 0) {
+                man.setDead(man.isDead());
+                System.out.println("Human's health: " + man.getHealth());
+                System.out.println("Human has died!");
+                break;
+            } else {
+                man.shootAlien(predator);
+                System.out.println("Human shoots Alien second time");
+                System.out.println("Human's ammo:" + man.getAmmo());
+                System.out.println("Alien's health: " + predator.getHealth());
+                System.out.println("-----------------------");
             }
         }
     }
