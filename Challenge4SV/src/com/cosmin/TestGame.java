70acfecf5e3ad1a451fb9f56ed3f5fbdd6b3e957
CosminBM <cosmincoco88@gmail.com>
2019-02-01 13:17:16 +0200

    GameOver!

--- Challenge4SV/src/com/cosmin/TestGame.java
+++ Challenge4SV/src/com/cosmin/TestGame.java
@@ -11,11 +11,7 @@ public class TestGame {
                 System.out.println("Alien's energy:" + predator.getEnergy());
                 System.out.println("Energy is lower than 10, can't byte!");
                 break;
-            } else if (predator.getHealth() <= 0) {
-                System.out.println("Alien's health: " + predator.getHealth());
-                System.out.println("Alien has died!");
-                break;
-            } else {
+            } else if (man.getHealth() > 0 && predator.getHealth() > 0) {
                 predator.biteHuman(man);
                 System.out.println("Alien bites man");
                 System.out.println("Alien's energy:" + predator.getEnergy());
@@ -27,11 +23,7 @@ public class TestGame {
                 System.out.println("Human's ammo:" + man.getAmmo());
                 System.out.println("Ammo is lower than 15, can't shoot!");
                 break;
-            } else if (man.getHealth() <= 0) {
-                System.out.println("Human's health: " + man.getHealth());
-                System.out.println("Human has died!");
-                break;
-            } else {
+            } else if (predator.getHealth() > 0 && man.getHealth() > 0) {
                 man.shootAlien(predator);
                 System.out.println("Human shoots Alien first time");
                 System.out.println("Human's ammo:" + man.getAmmo());
@@ -43,17 +35,21 @@ public class TestGame {
                 System.out.println("Human's ammo:" + man.getAmmo());
                 System.out.println("Ammo is lower than 15, can't shoot!");
                 break;
-            } else if (man.getHealth() <= 0) {
-                System.out.println("Human's health: " + man.getHealth());
-                System.out.println("Human has died!");
-                break;
-            } else {
+            } else if (predator.getHealth() > 0 && man.getHealth() > 0) {
                 man.shootAlien(predator);
                 System.out.println("Human shoots Alien second time");
                 System.out.println("Human's ammo:" + man.getAmmo());
                 System.out.println("Alien's health: " + predator.getHealth());
                 System.out.println("-----------------------");
             }
+        }
+
+        if (predator.isDead()) {
+            System.out.println("Alien's health: " + predator.getHealth());
+            System.out.println("Alien has died!");
+        } else if (man.isDead()){
+            System.out.println("Human's health: " + man.getHealth());
+            System.out.println("Human has died!");
         }
     }
 }
