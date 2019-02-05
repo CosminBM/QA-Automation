525e74b1587b131bb1fcdafc4d291840f22eefff
CosminBM <cosmincoco88@gmail.com>
2019-02-05 11:44:31 +0200

    Update-2

--- Challenge5SV/src/main/java/Test.java
+++ Challenge5SV/src/main/java/Test.java
@@ -1,29 +1,52 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\Work\\QA-Automation-Java\\Challenge5SV\\chromedriver\\chromedriver.exe");
+        //Home path Chrome
+        //System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\Work\\QA-Automation-Java\\Challenge5SV\\chromedriver\\chromedriver.exe");
 
-        ChromeDriver testLogin = new ChromeDriver();
+        //Work path Chrome
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\cosmin.badescu\\Desktop\\QA-Automation-Java\\Challenge5SV\\chromedriver\\chromedriver.exe");
 
-        testLogin.get("http://automationpractice.com/index.php");
-        testLogin.manage().window().maximize();
+        ChromeDriver testLoginChrome = new ChromeDriver();
 
-        WebElement SignInButton = testLogin.findElement(By.className("login"));
+        testLoginChrome.get("http://automationpractice.com/index.php");
+        testLoginChrome.manage().window().maximize();
+
+        WebElement SignInButton = testLoginChrome.findElement(By.className("login"));
         SignInButton.click();
 
-        WebElement InputEmail = testLogin.findElement(By.id("email"));
+        WebElement InputEmail = testLoginChrome.findElement(By.id("email"));
         InputEmail.sendKeys("cosmincoco88@gmail.com");
 
-        WebElement InputPassword = testLogin.findElement(By.id("passwd"));
+        WebElement InputPassword = testLoginChrome.findElement(By.id("passwd"));
         InputPassword.sendKeys("test123");
 
-        WebElement SubmitLogin = testLogin.findElement(By.id("SubmitLogin"));
+        WebElement SubmitLogin = testLoginChrome.findElement(By.id("SubmitLogin"));
         SubmitLogin.click();
 
-       testLogin.quit();
+        testLoginChrome.quit();
+
+       //Work path Firefox
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\cosmin.badescu\\Desktop\\QA-Automation-Java\\Challenge5SV\\geckodriver\\geckodriver.exe");
+
+        FirefoxDriver testLoginFirefox = new FirefoxDriver();
+
+        testLoginFirefox.get("http://automationpractice.com/index.php");
+        testLoginFirefox.manage().window().maximize();
+
+        testLoginFirefox.findElement(By.xpath("/html/body/div[1]/div[1]/header/div[2]/div/div/nav/div[1]/a")).click();
+
+        testLoginFirefox.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("cosmincoco88@gmail.com");
+
+        testLoginFirefox.findElement(By.xpath("//*[@id=\"passwd\"]")).sendKeys("test123");
+
+        testLoginFirefox.findElement(By.xpath("//*[@id=\"SubmitLogin\"]")).click();
+
+        testLoginFirefox.quit();
+
     }
 }
