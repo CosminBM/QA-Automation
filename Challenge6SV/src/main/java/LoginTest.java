bd92cc97d1974ce83f6f87f9f1ea8c74acdff8db
CosminBM <cosmincoco88@gmail.com>
2019-02-12 14:55:38 +0200

    Commit-2

--- Challenge6SV/src/main/java/LoginTest.java
+++ Challenge6SV/src/main/java/LoginTest.java
@@ -11,12 +11,12 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 
-    ChromeDriver driverChrome;
+   public ChromeDriver driverChrome;
 
     @BeforeClass
     public void openURL(){
         System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
-        ChromeDriver driverChrome = new ChromeDriver();
+        driverChrome = new ChromeDriver();
         driverChrome.get("http://automationpractice.com/index.php");
         driverChrome.manage().window().maximize();
         WebElement signInButtonChrome = driverChrome.findElement(By.className("login"));
@@ -24,45 +24,16 @@ public class LoginTest {
         System.out.println("Open the login URL before test");
     }
 
-//    @Test
-//    public void testInvalidPassword(){
-//        System.out.println("Test invalid Password");
-//        WebElement inputEmail = driverChrome.findElement(By.id("email"));
-//        inputEmail.sendKeys("cosmincoco88@gmail.com");
-//        Assert.assertEquals("cosmincoco88@gmail.com","cosmincoco88@gmail.com");
-//
-//        WebElement inputPassword = driverChrome.findElement(By.id("passwd"));
-//        inputPassword.sendKeys("test1234");
-//        Assert.assertEquals("test1234", "test123");
-//
-//        WebElement submitLogin = driverChrome.findElement(By.id("SubmitLogin"));
-//        submitLogin.click();
-//    }
-//
-//    @Test
-//    public void testInvalidEmail(){
-//        System.out.println("Test invalid Email");
-//        WebElement inputEmail = driverChrome.findElement(By.id("email"));
-//        inputEmail.sendKeys("cosmincoco886@gmail.com");
-//        Assert.assertEquals("cosmincoco886@gmail.com","cosmincoco88@gmail.com");
-//
-//        WebElement inputPassword = driverChrome.findElement(By.id("passwd"));
-//        inputPassword.sendKeys("test123");
-//        Assert.assertEquals("test123", "test123");
-//
-//        WebElement submitLogin = driverChrome.findElement(By.id("SubmitLogin"));
-//        submitLogin.click();
-//
-//    }
-
-    @Test
+    @Test(priority = 3)
     public void testValidLogin(){
         System.out.println("Test valid Login");
         WebElement inputEmail = driverChrome.findElement(By.id("email"));
+        inputEmail.clear();
         inputEmail.sendKeys("cosmincoco88@gmail.com");
         Assert.assertEquals("cosmincoco88@gmail.com","cosmincoco88@gmail.com");
 
         WebElement inputPassword = driverChrome.findElement(By.id("passwd"));
+        inputPassword.clear();
         inputPassword.sendKeys("test123");
         Assert.assertEquals("test123", "test123");
 
@@ -71,10 +42,43 @@ public class LoginTest {
 
     }
 
+    @Test(priority = 1)
+    public void testInvalidPassword(){
+        System.out.println("Test invalid Password");
+        WebElement inputEmail = driverChrome.findElement(By.id("email"));
+        inputEmail.clear();
+        inputEmail.sendKeys("cosmincoco88@gmail.com");
+        Assert.assertEquals("cosmincoco88@gmail.com","cosmincoco88@gmail.com");
+
+        WebElement inputPassword = driverChrome.findElement(By.id("passwd"));
+        inputPassword.clear();
+        inputPassword.sendKeys("test1234");
+        Assert.assertEquals("test1234", "test1234");
+
+        WebElement submitLogin = driverChrome.findElement(By.id("SubmitLogin"));
+        submitLogin.click();
+    }
+
+    @Test(priority = 2)
+    public void testInvalidEmail(){
+        System.out.println("Test invalid Email");
+        WebElement inputEmail = driverChrome.findElement(By.id("email"));
+        inputEmail.clear();
+        inputEmail.sendKeys("cosmincoco886@gmail.com");
+        Assert.assertEquals("cosmincoco886@gmail.com","cosmincoco886@gmail.com");
+
+        WebElement inputPassword = driverChrome.findElement(By.id("passwd"));
+        inputPassword.clear();
+        inputPassword.sendKeys("test123");
+        Assert.assertEquals("test123", "test123");
+
+        WebElement submitLogin = driverChrome.findElement(By.id("SubmitLogin"));
+        submitLogin.click();
+    }
+
     @AfterClass
     public void closeWebPage(){
         driverChrome.quit();
         System.out.println("Quit Chrome");
     }
-
 }
